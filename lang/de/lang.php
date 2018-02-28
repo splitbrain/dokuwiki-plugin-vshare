13c826e41582cf14d84299aa8a15ad72fc3cf5be
F. Mueller-Donath <j.felix@mueller-donath.de>
2018-02-28 11:30:41 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,9 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-$lang['js']['button']          = 'Video von Videoplatformen einfügen';
+$lang['js']['button']          = 'Video von Videoplattformen einfügen';
 $lang['js']['prompt']          = 'Bitte fügen Sie hier die komplette URL zur Videoseite ein:';
 $lang['js']['notfound']        = 'Die URL wurde leider nicht erkannt.
-Bitte schauen Sie in der Dokumentation wie Sie die korrekte Syntax für dieses Video manuell eingeben können.';
+Bitte lesen Sie in der Dokumentation nach wie Sie die korrekte Syntax für dieses Video manuell eingeben können.';
