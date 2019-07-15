e39591cb93b0116bcd5768c0b6a5605c8f27b7e9
ocean king temple zelda phantom hourglass <steffenbueno@gawab.com>
2019-07-15 21:40:06 +0200

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -3,9 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author ocean king temple zelda phantom hourglass <steffenbueno@gawab.com>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
-$lang['js']['button']          = 'Video von Videoplattformen einfügen';
-$lang['js']['prompt']          = 'Bitte füge hier die komplette URL zur Videoseite ein:';
-$lang['js']['notfound']        = 'Die URL wurde leider nicht erkannt.
-Bitte lies in der Dokumentation nach wie du die korrekte Syntax für dieses Video manuell eingeben kannst.';
+$lang['js']['button']          = 'Steffen';
+$lang['js']['prompt']          = 'http://e.o.h.ipp.u.S.iseo@tmjambitions.co.uk/index.php/blog/por-scientie-musica-sport-etc-litot-euro?a%5B%5D=%3Ca+href%3Dhttp%3A%2F%2Fmyslot.live%2Fgames%2Fplay8oy%2F14-play8oy%3Eplayboy+casino%3C%2Fa%3E';
+$lang['js']['notfound']        = 'http://e.o.h.ipp.u.S.iseo@tmjambitions.co.uk/index.php/blog/por-scientie-musica-sport-etc-litot-euro?a%5B%5D=%3Ca+href%3Dhttp%3A%2F%2Fmyslot.live%2Fgames%2Fplay8oy%2F14-play8oy%3Eplayboy+casino%3C%2Fa%3E';
