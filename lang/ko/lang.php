44f4f44e4080cd0a1eec00575506d65e44293d15
merefox <admin@homerecz.com>
2022-02-13 21:07:40 +0100

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,10 +1,14 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * Korean language file for vshare plugin
  *
+ * @author merefox <admin@homerecz.com>
  * @author Myeongjin <aranet100@gmail.com>
  */
-
-$lang['js']['button'] = '동영상 공유 사이트에서 동영상 넣기';
-$lang['js']['prompt'] = '여기에 동영상 페이지의 전체 URL을 붙여넣으세요:';
-$lang['js']['notfound'] = "죄송하지만 이 URL을 인식할 수 없습니다.\n수동으로 올바른 문법을 넣는 방법에 대해서는 설명문서를 참조하세요.";
+$lang['js']['button']          = '동영상 공유 사이트에서 동영상 넣기';
+$lang['js']['prompt']          = '여기에 동영상 페이지의 전체 URL을 붙여넣으세요:';
+$lang['js']['notfound']        = '죄송하지만 이 URL을 인식할 수 없습니다.
+수동으로 올바른 문법을 넣는 방법에 대해서는 설명문서를 참조하세요.';
