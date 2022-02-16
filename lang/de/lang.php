e92bcdc2aeaae1945b14ffce0471a1cce865a416
Joel Strasser <strasser999@gmail.com>
2022-02-16 15:20:05 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Joel Strasser <strasser999@gmail.com>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
@@ -10,3 +11,4 @@ $lang['js']['button']          = 'Video von Videoplattformen einfügen';
 $lang['js']['prompt']          = 'Bitte fügen Sie hier die komplette URL zur Videoseite ein:';
 $lang['js']['notfound']        = 'Die URL wurde leider nicht erkannt.
 Bitte lesen Sie in der Dokumentation nach wie Sie die korrekte Syntax für dieses Video manuell eingeben können.';
+$lang['js']['click']           = 'Klicken Sie, um dieses Video zu laden. Es werden Ihre IP-Adresse und eventuell andere Daten zu %s geschickt.';
