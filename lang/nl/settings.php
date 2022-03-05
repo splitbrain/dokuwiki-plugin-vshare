fcc05420572dc1b84c604d2089d7f2bcdcabf88e
Gerrit <klapinklapin@gmail.com>
2022-03-05 15:55:07 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gerrit <klapinklapin@gmail.com>
+ */
+$lang['gdpr']                  = 'Klik-om-te-laden mechanisme aanzetten';
+$lang['debug']                 = 'Schakel de ~~vshare-debug~~ syntax in om visueel alle geconfigureerde site in één keer te controleren op fouten.';
