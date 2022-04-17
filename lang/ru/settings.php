a276c0cb664766419ebce35684e2e188d68533fa
Iliya <iliyabylich04@gmail.com>
2022-04-17 12:45:30 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Iliya <iliyabylich04@gmail.com>
+ */
+$lang['gdpr']                  = 'Включение нажатие-на-загрузку механизма.';
+$lang['debug']                 = 'Включить ~~vshare-debug~~ символы на визуальную сборку все проконфегурированы сайты';
