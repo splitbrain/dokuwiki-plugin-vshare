4f528acbee3cab31f7f8784bc9d7c3bdfc5fc3dd
铁桶 <2335652849@qq.com>
2022-05-22 03:55:26 +0200

    translation update

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 铁桶 <2335652849@qq.com>
+ */
+$lang['js']['click']           = '點擊以載入此影片。您的IP位址和其他資料（可能）將被傳輸至%s。';
