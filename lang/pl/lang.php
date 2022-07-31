59b7cbf1769feaea1f1510b31509d9ae2f351b33
Marek Adamski <fevbew@wp.pl>
2022-08-01 01:30:05 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,9 +3,11 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author Bartek S <sadupl@gmail.com>
  */
 $lang['js']['button']          = 'Wstaw wideo z witryn udostępniających wideo';
-$lang['js']['prompt']          = 'Wklej tutaj pełny adres URL do wideo:';
-$lang['js']['notfound']        = 'Przepraszamy, ten URL nie został rozpoznany.
-Proszę zapoznaj się z dokumentacją jak ręcznie wstawić poprawną składnię.';
+$lang['js']['prompt']          = 'Wklej tutaj pełny adres URL strony wideo:';
+$lang['js']['notfound']        = 'Przepraszamy, ten adres URL nie został rozpoznany.
+Zapoznaj się z dokumentacją, jak ręcznie wstawiać poprawną składnię.';
+$lang['js']['click']           = 'Kliknij, aby wczytać to wideo. Twój adres IP i ewentualnie inne dane zostaną przekazane do %s.';
