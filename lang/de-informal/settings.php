54b934b3e207ded6c1a10070ca2f44485de628de
Hella <hella.breitkopf@gmail.com>
2022-08-05 14:15:16 +0200

    translation update

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
+ */
+$lang['gdpr']                  = '"Klicken zum Laden" einschalten';
+$lang['debug']                 = 'Aktiviere die ~~vshare-debug~~ Syntax um alle konfigurierten Seiten visuell zu debuggen';
