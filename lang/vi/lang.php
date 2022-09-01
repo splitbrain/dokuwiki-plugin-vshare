bdc6fcb9d59e630e3265f94d7f016e35688a2a3a
Thien Hau <thienhau.9a14@gmail.com>
2022-09-01 07:15:11 +0200

    translation update

--- lang/vi/lang.php
+++ lang/vi/lang.php
@@ -3,9 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Thien Hau <thienhausoftware@gmail.com>
+ * @author Thien Hau <thienhau.9a14@gmail.com>
  */
 $lang['js']['button']          = 'Chèn video từ các trang chia sẻ video';
 $lang['js']['prompt']          = 'Vui lòng dán URL đầy đủ vào trang video ở đây:';
 $lang['js']['notfound']        = 'Xin lỗi, URL này không được công nhận.
 Vui lòng tham khảo tài liệu về cách chèn đúng cú pháp.';
+$lang['js']['click']           = 'Nhấn để tải video này. Địa chỉ IP của bạn và có thể cả dữ liệu khác sẽ được chuyển đến %s.';
