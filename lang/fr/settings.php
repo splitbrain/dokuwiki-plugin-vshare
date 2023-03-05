91b410294b366bbddf148b1d10c6c87946dc1ab2
Fabrice Dejaigher <fabrice@chtiland.com>
2023-03-05 18:30:22 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,6 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
  * @author Mabrouk Belhout <mabroukb@gmail.com>
  */
 $lang['gdpr']                  = 'Activer le mécanisme click-to-load';
+$lang['debug']                 = 'Activer la syntaxe ~~vshare-debug~~ pour "deboguer" tous les sites configurés.';
