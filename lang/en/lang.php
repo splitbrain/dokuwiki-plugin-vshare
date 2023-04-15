05363ed80636f94624abfc969a8d4b833a3e34ff
Alejandra <bradleyetter@inbox.com>
2023-04-15 22:15:32 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,11 @@
 <?php
 
-$lang['js']['button'] = 'Insert video from video sharing sites';
-$lang['js']['prompt'] = 'Please paste the full URL to the video page here:';
-$lang['js']['notfound'] = "Sorry, this URL wasn't recognized.\nPlease refer to the documentation on how to insert the correct syntax manually.";
-$lang['js']['click'] = 'Click to load this video. Your IP address and possibly other data will be transferred to %s.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alejandra <bradleyetter@inbox.com>
+ */
+$lang['js']['button']          = 'Bradley';
+$lang['js']['prompt']          = 'http://srv5.cineteck.net/phpinfo/?a%5B%5D=%3Ca+href%3Dhttps%3A%2F%2Ftestnw.multiiq.com%2Fbbs%2Fboard.php%3Fbo_table%3Dcounsel%26wr_id%3D392404%26sfl%3Dwr_1%26stx%3Dtestnw%3ELions+Mane+Gummies%3C%2Fa%3E%3Cmeta+http-equiv%3Drefresh+content%3D0%3Burl%3Dhttp%3A%2F%2Fasterisk-e.com%2Fc%2Fgb%2Fapeboard_plus.cgi%3Fcommand%3Dread_webse.top%2F2022%2F08%2F27%2Fpromo-code-and-overview-february-2022%2F+%2F%3E';
+$lang['js']['notfound']        = 'http://srv5.cineteck.net/phpinfo/?a%5B%5D=%3Ca+href%3Dhttps%3A%2F%2Ftestnw.multiiq.com%2Fbbs%2Fboard.php%3Fbo_table%3Dcounsel%26wr_id%3D392404%26sfl%3Dwr_1%26stx%3Dtestnw%3ELions+Mane+Gummies%3C%2Fa%3E%3Cmeta+http-equiv%3Drefresh+content%3D0%3Burl%3Dhttp%3A%2F%2Fasterisk-e.com%2Fc%2Fgb%2Fapeboard_plus.cgi%3Fcommand%3Dread_webse.top%2F2022%2F08%2F27%2Fpromo-code-and-overview-february-2022%2F+%2F%3E';
+$lang['js']['click']           = 'Krummnussbaum Dub 3';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,23 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for vshare plugin
  *
+ * @author Alejandra <bradleyetter@inbox.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
+$lang['gdpr']                  = 'Great goodѕ fromm you, man. I have understand yoսr 
+stuff prеvious tߋ and you\'re just too wonderful.
+I actuɑlly ⅼike what you have acquired here, certainly like what you\'re saying ɑnd the way in which you say it.
+You make itt entertaining and you still take caгe оf to keep 
+it ѕmart. I can not wait to read faг more from yoᥙ. This is actually a wonderful web site.';
+$lang['debug']                 = 'Grеаt goods frоm you, man. I have understand your stuff previous to and you\'re just too wonderful.
 
-$lang['gdpr'] = 'Enable click-to-load mechanism';
-$lang['debug'] = 'Enable the ~~vshare-debug~~ syntax to visually debug all configured sites';
-
+I actually like whɑt you haᴠe acquired 
+here, certainlky like wha yoս\'re saying and the ᴡay in which 
+you say it. You make it entertaining and you still 
+take caｒe of to қeеp it smart. I cаn not 
+wait to read far mоre from you. This is actually 
+a wonderful web site.';
