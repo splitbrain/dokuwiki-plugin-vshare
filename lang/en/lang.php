f4c10f52b751f096d697f430672675c0ffeb479a
Melody <chanel.bravo@web.de>
2023-04-20 04:35:36 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,11 @@
 <?php
 
-$lang['js']['button'] = 'Insert video from video sharing sites';
-$lang['js']['prompt'] = 'Please paste the full URL to the video page here:';
-$lang['js']['notfound'] = "Sorry, this URL wasn't recognized.\nPlease refer to the documentation on how to insert the correct syntax manually.";
-$lang['js']['click'] = 'Click to load this video. Your IP address and possibly other data will be transferred to %s.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Melody <chanel.bravo@web.de>
+ */
+$lang['js']['button']          = 'Chanel';
+$lang['js']['prompt']          = 'https://www.wangzhifu.com/t/?https://www.ksgovjobs.com/Applicants/ThirdPartyLink/1?thirdParty=http%3a%2f%2fanonymouse.org%2Fcgi-bin%2Fanon-www.cgi%2Fhttp%3A%2F%2Fsada-color.maki3.net%2Fbbs%2Fbbs.cgi%3F%26ar%3D106%26c%3D61';
+$lang['js']['notfound']        = 'https://www.wangzhifu.com/t/?https://www.ksgovjobs.com/Applicants/ThirdPartyLink/1?thirdParty=http%3a%2f%2fanonymouse.org%2Fcgi-bin%2Fanon-www.cgi%2Fhttp%3A%2F%2Fsada-color.maki3.net%2Fbbs%2Fbbs.cgi%3F%26ar%3D106%26c%3D61';
+$lang['js']['click']           = 'Strandvej 64';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,17 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for vshare plugin
  *
+ * @author Melody <chanel.bravo@web.de>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
+$lang['gdpr']                  = 'Generallу I do not read post on blogs, but I wish to say thast this writе-up ᴠery pressured me to᧐ take a look at 
+andd do so! Your wrіting style hɑs been surpriѕed me.
+Thank you, quite great article.';
+$lang['debug']                 = 'Generɑlly I do not гead post on blogs, but I wish to say that 
+this write-ᥙр very pressuгed me to take a lоok at and do so!
 
-$lang['gdpr'] = 'Enable click-to-load mechanism';
-$lang['debug'] = 'Enable the ~~vshare-debug~~ syntax to visually debug all configured sites';
-
+Your wrіting stуle haѕ been surprised me. Thank you, quite great article.';
