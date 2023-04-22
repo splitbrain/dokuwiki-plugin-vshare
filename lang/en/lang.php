0f3a4030db07a66db7c89af57f981eaf1668ef45
naturestears.com <marilynmitchell@yahoo.com>
2023-04-22 03:55:11 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,11 @@
 <?php
 
-$lang['js']['button'] = 'Insert video from video sharing sites';
-$lang['js']['prompt'] = 'Please paste the full URL to the video page here:';
-$lang['js']['notfound'] = "Sorry, this URL wasn't recognized.\nPlease refer to the documentation on how to insert the correct syntax manually.";
-$lang['js']['click'] = 'Click to load this video. Your IP address and possibly other data will be transferred to %s.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author naturestears.com <marilynmitchell@yahoo.com>
+ */
+$lang['js']['button']          = 'Marilyn';
+$lang['js']['prompt']          = 'http://naturestears.com/php/Test.php?a%5B%5D=%3Ca+href%3Dhttps%3A%2F%2Fwakeuplaughing.com%2Fphpinfo.php%3Fa%255B%255D%3D%253Ca%2Bhref%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%253ETrident%2Bcod%2Bgummies%253C%252Fa%253E%253Cmeta%2Bhttp-equiv%253Drefresh%2Bcontent%253D0%253Burl%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%2B%252F%253E%3ETrident+cod+gummies%3C%2Fa%3E%3Cmeta+http-equiv%3Drefresh+content%3D0%3Burl%3Dhttp%3A%2F%2Fstatic.3.203.76.144.clients.your-server.de%2Fphpinfo.php%3Fa%255B%255D%3D%253Ca%2Bhref%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%253Enatural%2Btreatment%2Bfor%2Bdogs%2Bwith%2Bbladder%2Bcancer%253C%252Fa%253E%253Cmeta%2Bhttp-equiv%253Drefresh%2Bcontent%253D0%253Burl%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%2B%252F%253E+%2F%3E';
+$lang['js']['notfound']        = 'http://naturestears.com/php/Test.php?a%5B%5D=%3Ca+href%3Dhttps%3A%2F%2Fwakeuplaughing.com%2Fphpinfo.php%3Fa%255B%255D%3D%253Ca%2Bhref%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%253ETrident%2Bcod%2Bgummies%253C%252Fa%253E%253Cmeta%2Bhttp-equiv%253Drefresh%2Bcontent%253D0%253Burl%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%2B%252F%253E%3ETrident+cod+gummies%3C%2Fa%3E%3Cmeta+http-equiv%3Drefresh+content%3D0%3Burl%3Dhttp%3A%2F%2Fstatic.3.203.76.144.clients.your-server.de%2Fphpinfo.php%3Fa%255B%255D%3D%253Ca%2Bhref%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%253Enatural%2Btreatment%2Bfor%2Bdogs%2Bwith%2Bbladder%2Bcancer%253C%252Fa%253E%253Cmeta%2Bhttp-equiv%253Drefresh%2Bcontent%253D0%253Burl%253Dhttps%253A%252F%252Ftridentcbdsgummies.com%2B%252F%253E+%2F%3E';
+$lang['js']['click']           = 'Elleno 38';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,13 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for vshare plugin
  *
+ * @author naturestears.com <marilynmitchell@yahoo.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-
-$lang['gdpr'] = 'Enable click-to-load mechanism';
-$lang['debug'] = 'Enable the ~~vshare-debug~~ syntax to visually debug all configured sites';
-
+$lang['gdpr']                  = 'Soome ｒealoly іnteresting informatіon, welpl written annd 
+generally user pleasant.';
+$lang['debug']                 = 'Sоmee resally interesting information, well written and generally user pleasant.';
