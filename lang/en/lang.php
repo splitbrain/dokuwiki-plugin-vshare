9a09406eeaf6156853ffc57a338ff640664103a8
https://avtosubmitter.fun/ssylki-vysokogo-kachestva <christena.kyngdon@gmail.com>
2023-04-30 09:40:08 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,11 @@
 <?php
 
-$lang['js']['button'] = 'Insert video from video sharing sites';
-$lang['js']['prompt'] = 'Please paste the full URL to the video page here:';
-$lang['js']['notfound'] = "Sorry, this URL wasn't recognized.\nPlease refer to the documentation on how to insert the correct syntax manually.";
-$lang['js']['click'] = 'Click to load this video. Your IP address and possibly other data will be transferred to %s.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author https://avtosubmitter.fun/ssylki-vysokogo-kachestva <christena.kyngdon@gmail.com>
+ */
+$lang['js']['button']          = 'Christena';
+$lang['js']['prompt']          = 'https://avtosubmitter.fun/ssylki-vysokogo-kachestva/';
+$lang['js']['notfound']        = 'https://avtosubmitter.fun/ssylki-vysokogo-kachestva/';
+$lang['js']['click']           = 'Gruenauer Strasse 15';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,20 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for vshare plugin
  *
+ * @author https://avtosubmitter.fun/ssylki-vysokogo-kachestva <christena.kyngdon@gmail.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
+$lang['gdpr']                  = 'Admiring the dedication you put into your website and in depth information you offer.
+It\'s nice to come across a blog every once in a while that isn\'t the same 
+unwanted rehashed information. Excellent read!
 
-$lang['gdpr'] = 'Enable click-to-load mechanism';
-$lang['debug'] = 'Enable the ~~vshare-debug~~ syntax to visually debug all configured sites';
-
+I\'ve bookmarked your site and I\'m including your RSS feeds to my Google 
+account.';
+$lang['debug']                 = 'Admiring the dedication you put into your website and in depth information you offer.
+It\'s nice to come across a blog every once 
+in a while that isn\'t the same unwanted rehashed information. Excellent read!
+I\'ve bookmarked your site and I\'m including your RSS feeds to my Google account.';
