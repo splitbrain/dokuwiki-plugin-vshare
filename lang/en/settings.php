dee35ae12f0d349d57b5c66c2baaafb47a975529
https://linksbuilding.fun <felixpritchard@inbox.com>
2023-05-09 04:50:35 +0200

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,15 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for vshare plugin
  *
+ * @author https://linksbuilding.fun <felixpritchard@inbox.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-
-$lang['gdpr'] = 'Enable click-to-load mechanism';
-$lang['debug'] = 'Enable the ~~vshare-debug~~ syntax to visually debug all configured sites';
-
+$lang['gdpr']                  = 'You have made some decent points there. I 
+checked on the net for additional information about the 
+issue and found most individuals will go along with your views on this web site.';
+$lang['debug']                 = 'You have made some decent points there. I checked on the net for additional information about the issue and found most individuals 
+will go along with your views on this web site.';
