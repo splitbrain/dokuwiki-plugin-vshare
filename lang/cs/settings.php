d78e229b279e14893336ea811c43a3b09691b190
Martin Růžička <martinr@post.cz>
2025-02-09 12:30:06 +0100

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Martin Růžička <martinr@post.cz>
+ */
+$lang['gdpr']                  = 'Povolit mechanismus click-to-load (klikněte pro načtení)';
+$lang['extrahard']             = 'Povolit rozšířené funkce ochrany soukromí pro vložené soubory, čímž zakážete funkce jako prémiové odběry, sledovat později atd.';
+$lang['debug']                 = 'Povolit syntaxi ~~vshare-debug~~ pro vizuální ladění všech nakonfigurovaných webů';
