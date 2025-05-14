d37c81d899f2ed9cff11858352d4ad739829a549
Pablo <tuthotep@gmail.com>
2025-05-14 05:00:20 +0200

    translation update

---
+++ lang/es/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Pablo <tuthotep@gmail.com>
+ */
+$lang['gdpr']                  = 'Activar el "click-to-load"';
