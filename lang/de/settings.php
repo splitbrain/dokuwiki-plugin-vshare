378ce967eecbcddc6d44223feec35f966ab626ae
andreash <aeheil@gmail.com>
2025-05-20 10:35:17 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author andreash <aeheil@gmail.com>
  * @author Joel Strasser <strasser999@gmail.com>
  */
 $lang['gdpr']                  = 'Click-to-Load-Mechanismus aktivieren';
+$lang['extrahard']             = 'Aktiviere die erweiterten Datenschutzfunktionen für die eingebetteten Videos. Dadurch werden Funktionen wie Premium-Abonnements, „Später ansehen“ und andere deaktiviert.';
 $lang['debug']                 = 'Aktivieren Sie die ~~vshare-debug~~-Syntax um alle konfigurierten Seiten visuell zu untersuchen';
