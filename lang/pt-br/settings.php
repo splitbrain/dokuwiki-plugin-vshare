afbb9a2c507960851e4a8ef78454d551ebad9d8b
Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
2025-09-03 00:15:45 +0200

    translation update

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
  * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  */
 $lang['gdpr']                  = 'Ativar mecanismo de clique para carregar';
+$lang['extrahard']             = 'Ative os recursos de privacidade aprimorados para as incorporações, isso desativará recursos como assinaturas premium, assistir mais tarde, etc.';
 $lang['debug']                 = 'Habilita a sintaxe ~~vshare-debug~~ para depurar visualmente todos os sites configurados';
