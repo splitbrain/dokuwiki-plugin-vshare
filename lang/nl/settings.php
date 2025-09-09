2a530467d1f9c9ce19696e4b91b048c66efbb67b
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-09-09 14:02:29 +0200

    Add Dutch translation for enhanced privacy features (extrahard)

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -6,4 +6,5 @@
  * @author Gerrit <klapinklapin@gmail.com>
  */
 $lang['gdpr']                  = 'Klik-om-te-laden mechanisme aanzetten';
+$lang['extrahard']             = 'Schakel de verbeterde privacyfuncties in voor de ingesloten video's. Hiermee worden functies zoals premium-abonnementen, later bekijken, enz. uitgeschakeld.';
 $lang['debug']                 = 'Schakel de ~~vshare-debug~~ syntax in om visueel alle geconfigureerde site in één keer te controleren op fouten.';
