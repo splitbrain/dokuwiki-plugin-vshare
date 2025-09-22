1dae3761b6db3dfc1b3cdde7d07557aaab22961d
smocap <smocap@gmail.com>
2025-09-22 06:30:18 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -3,11 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author smocap <smocap@gmail.com>
  * @author Pablo <tuthotep@gmail.com>
  * @author Domingo Redal <docxml@gmail.com>
  */
 $lang['js']['button']          = 'Insertar vídeo desde un sitio dedicado a compartir vídeos';
-$lang['js']['prompt']          = 'Pegue la dirección URL completa de la página del vídeo aquí:';
+$lang['js']['prompt']          = 'Pega la dirección URL completa de la página del vídeo aquí:';
 $lang['js']['notfound']        = 'Lo sentimos, la dirección URL no fue reconocida.
 Consulte la documentación sobre cómo insertar manualmente la sintaxis correcta.';
-$lang['js']['click']           = 'Click para cargar el video. Si dirección IP y posiblemente algún otro data serán transferidos a %s.';
+$lang['js']['click']           = 'Clic para cargar este video. Tu dirección IP y posiblemente otros datos serán transferidos a %s.';
--- lang/es/settings.php
+++ lang/es/settings.php
@@ -3,6 +3,9 @@
 /**
  * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
  *
+ * @author smocap <smocap@gmail.com>
  * @author Pablo <tuthotep@gmail.com>
  */
-$lang['gdpr']                  = 'Activar el "click-to-load"';
+$lang['gdpr']                  = 'Activar el mecanismo de carga al hacer clic';
+$lang['extrahard']             = 'Activa las funciones de privacidad mejorada para los elementos incrustados, esto desactivará opciones como suscripciones premium, ver más tarde, etc.';
+$lang['debug']                 = 'Activa la sintaxis ~~vshare-debug~~ para depurar visualmente todos los sitios configurados';
