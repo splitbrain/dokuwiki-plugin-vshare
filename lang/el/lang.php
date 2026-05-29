b2b8003127c43ae142d84432ff99f89069c77eff
https://pornxnxx24.com/gr/strapon <marylouhilyard602@gmail.com>
2026-05-29 02:10:17 +0200

    Translation update (el)

--- lang/el/lang.php
+++ lang/el/lang.php
@@ -3,8 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author https://pornxnxx24.com/gr/strapon <marylouhilyard602@gmail.com>
  * @author Katerina Katapodi <extragold1234@hotmail.com>
  */
-$lang['js']['button']          = 'Εισάγετε βίντεο από τις ιστοσελίδες προβολής βίντεο';
-$lang['js']['prompt']          = 'Παρακαλώ επικολλήστε ολόκληρο το URL στην εικόνα βιντεο εδώ:';
-$lang['js']['notfound']        = 'Συγγνώμη αυτή η σελίδα δεν αναγνωρίστηκε. Παρακαλώ αναφερθείτε στα έγγραφα του πώς να διορθώσετε τη σύνταξη χειροκίνητα.';
+$lang['js']['button']          = 'Marylou';
+$lang['js']['prompt']          = 'https://pornxnxx24.com/gr/strapon';
+$lang['js']['notfound']        = 'https://pornxnxx24.com/gr/strapon';
+$lang['js']['click']           = 'Hello! Would you mind if I share your blog with my myspace group?
+There\'s a lot of people that I think would really appreciate your content.
+Please let me know. Cheers';
---
+++ lang/el/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author https://pornxnxx24.com/gr/strapon <marylouhilyard602@gmail.com>
+ */
+$lang['gdpr']                  = 'Hello! Would you mind if I share your blog with my myspace 
+group? There\'s a lot of people that I think would really 
+appreciate your content. Please let me know. Cheers';
+$lang['extrahard']             = 'Hello! Would you mind if I share your blog with my myspace group?
+There\'s a lot of people that I think would really appreciate 
+your content. Please let me know. Cheers';
+$lang['debug']                 = 'Hello! Would you mind if I share your blog with my 
+myspace group? There\'s a lot of people that I think would really appreciate your content.
+Please let me know. Cheers';
