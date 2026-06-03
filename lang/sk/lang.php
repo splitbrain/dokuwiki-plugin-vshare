17e252cb7b79ebeeddae31cf0110a72c8cce8741
Wizzard <wizzardsk@gmail.com>
2026-06-03 19:50:22 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['js']['button']          = 'Vložiť video zo služieb na zdieľanie videí';
+$lang['js']['prompt']          = 'Sem vložte úplnú URL adresu stránky s videom:';
+$lang['js']['notfound']        = 'Prepáčte, táto URL adresa nebola rozpoznaná.
+Postup, ako vložiť správnu syntax ručne, nájdete v dokumentácii.';
+$lang['js']['click']           = 'Kliknutím načítate toto video. Vaša IP adresa a prípadne ďalšie údaje budú prenesené na %s.';
---
+++ lang/sk/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['gdpr']                  = 'Povoliť mechanizmus načítania kliknutím';
+$lang['extrahard']             = 'Povoliť rozšírené funkcie ochrany súkromia pre vložené videá; vypne to funkcie ako prémiové predplatné, sledovať neskôr a pod.';
+$lang['debug']                 = 'Povoliť syntax ~~vshare-debug~~ na vizuálne ladenie všetkých nakonfigurovaných služieb';
